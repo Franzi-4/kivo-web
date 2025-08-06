1eb9074cefd711aee4b78b4ace597bc262b1938d
Franzi-4 <harzheimfranzi@gmail.com>
2025-08-06 21:22:22 +0200

    initiate gh page

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.PROD ? '/fintech-template-0729' : ''}>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/blog" element={<Blog />} />
--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/fintech-template-0729/' : '/',
   server: {
     host: "::",
     port: 8080,
