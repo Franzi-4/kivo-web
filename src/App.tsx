32649b029d9b4fab0ddef3110f25ead706d677f8
Franzi-4 <harzheimfranzi@gmail.com>
2025-08-06 22:13:25 +0200

    Fix asset paths for custom domain deployment

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename={import.meta.env.PROD ? '/kivo-web' : ''}>
+      <BrowserRouter basename={import.meta.env.PROD ? '' : ''}>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/blog" element={<Blog />} />
